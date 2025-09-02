bc225bdbb50b7afffd790d9b3aa0ec1047fa53c0
Robert Cooper <rcooper@gmail.com>
2025-09-02 16:48:32 -0400

    Updated a few things

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -3,23 +3,23 @@ import { useEffect, useState, useMemo } from "react";
 const GALLERY_TAGS = ["All", "Lathe","Mill", "Mill/Turn", "5-Axis", "Swiss", "Aluminum", "Stainless", "Titanium","Plastic"];
 
 const gallery = [
-  { img: "/img/gallery/MB1.png", h: "Stainless Muzzle Brake", d: "Tight concentricity, Engraving, Angled Teeth", tags: ["Mill/Turn", "Stainless"] },
-  { img: "/img/gallery/Acrylic.png", h: "Acrylic", d: "Demo Part to show inter-workings", tags: ["Lathe", "Plastic"] },
-  { img: "/img/gallery/Connector.png", h: "Stainless Firearm Connecter", d: "Acme ID Thread Double Turret Machine", tags: ["Mill/Turn", "Stainless"] },
+  { img: "/img/gallery/MB1.png", h: "Muzzle Brake", d: "Tight concentricity, Engraving, Angled Teeth 17-4", tags: ["Mill/Turn", "Stainless"] },
+  { img: "/img/gallery/Acrylic.png", h: "Acrylic Demo Part", d: "Demo Part to show inter-workings", tags: ["Lathe", "Plastic"] },
+  { img: "/img/gallery/Connector.png", h: "Firearm Connecter", d: "Acme ID Thread Double Turret Machine", tags: ["Mill/Turn", "Stainless"] },
   { img: "/img/gallery/Swiss4.png", h: "Aerspace Tooling", d: "Swiss Part out of A2", tags: ["Swiss"] },
   { img: "/img/gallery/Flange.png", h: "Custom Automotive Part", d: "Intake part made from 6061", tags: ["Lathe","Aluminum"] },
   { img: "/img/gallery/Diffuser.jpg", h: "Firearm Part", d: "Diffuser 17-4", tags: ["Mill/Turn","Stainless"] },
   { img: "/img/gallery/Paint.jpg", h: "Automotive Paint Component", d: "Plastic with very tight GDT", tags: ["Mill/Turn","Plastic"] },
-  { img: "/img/gallery/Extractor.jpg", h: "Tank Turrent Part", d: "Made from Hardened Forging", tags: ["Mill/Turn"] },
+  { img: "/img/gallery/Extractor.jpg", h: "Tank Turrent Part", d: "Made from Hardened Forging Defnse part with very tight GDT", tags: ["Mill/Turn"] },
   { img: "/img/gallery/Segthrd.jpg", h: "Segment Thread Macro", d: "These were a test for my macro 8-32 10-32 3/8-24", tags: ["Swiss"] },
   { img: "/img/gallery/Plate.png", h: "Plate for Automotive Manufacturing", d: "Milled Aluminum Plate", tags: ["Mill","Aluminum"] },
   { img: "/img/gallery/Manual.jpg", h: "Manual Machining", d: "Hard Turn a relief for easy press fit", tags: ["Lathe"] },
   { img: "/img/gallery/Knurl.jpg", h: "Kurling", d: "Knurling a medical part with lower turret as a tailstock", tags: ["Lathe"] },
   { img: "/img/gallery/Pinchturn.jpg", h: "Pinch-Turning", d: "Pinch-Turning 13-8 Stainless Steal to save time", tags: ["Lathe"] },
-  { img: "/img/gallery/Tailstock.jpg", h: "Tailstock", d: "Using the Sub Spindle as a tailstock on a long part", tags: ["Lathe"] },
-  { img: "/img/gallery/Fiveaxis.jpg", h: "5 Axis Mill/Turn", d: "Angled drill in plastic", tags: ["5-Axis","Mill/Turn"] },
-  { img: "/img/gallery/Canoe.jpg", h: "5 Axis Mill/Turn Prototype", d: "Aluminum Prototype Part", tags: ["5-Axis","Mill/Turn"] },
-   { img: "/img/gallery/Mount.png", h: "Mill/Turn Production", d: "17-4 Part where 10,000 Pieces were required", tags: ["Mill/Turn", "Stainless"] },
+  { img: "/img/gallery/Tailstock.jpg", h: "Tailstock", d: "Using the Sub Spindle with live center as a tailstock on a long part", tags: ["Lathe"] },
+  { img: "/img/gallery/Fiveaxis.jpg", h: "5 Axis Mill/Turn", d: "Angled drill in plastic part was 7 ops reduced to 1", tags: ["5-Axis","Mill/Turn"] },
+  { img: "/img/gallery/Canoe.jpg", h: "5 Axis Mill/Turn Prototype", d: "Aluminum Prototype Part for automotive industry", tags: ["5-Axis","Mill/Turn"] },
+  { img: "/img/gallery/Mount.png", h: "Mill/Turn Production", d: "17-4 Part where 10,000 Pieces were required", tags: ["Mill/Turn", "Stainless"] },
   { img: "/img/gallery/Swiss5.png", h: "Brass Component", d: "5-Axis Swiss Part: 2 angled holes 1 with tap", tags: ["5-Axis","Swiss"] }
 ];
 
